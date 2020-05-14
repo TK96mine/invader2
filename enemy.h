ce2905e7f0fdfd946913890ae60ac4dc1306e811
K <k91maad@outlook.jp>
2020-05-14 13:58:12 +0900

    2020/5/14  13:58更新

--- enemy.h
+++ enemy.h
@@ -1,1 +1,40 @@
-#pragma once
+//定数
+
+//列挙型
+//敵の左右の移動用
+enum MOVE_LR
+{
+	MOVE_LR_LEFT,
+	MOVE_LR_RIGHT,
+	MOVE_LR_MAX
+};
+
+//敵の上下の移動用
+enum MOVE_MODE
+{
+	MOVE_MODE_LR,
+	MOVE_MODE_DOWN,
+	MOVE_MODE_MAX
+};
+
+//敵の種類
+enum ENEMY_TYPE
+{
+	ENEMY_TYPE_NON,
+	ENEMY_TYPE_RED,
+	ENEMY_TYPE_BLUE,
+	ENEMY_TYPE_GREEN,
+	ENEMY_TYPE_ORANGE,
+	ENEMY_TYPE_MAX
+};
+
+
+//構造体
+
+
+
+//プロトタイプ宣言
+void EnemySystemInit(void);	//敵の画像ロード用
+void EnemyGameInit(void);	//敵の変数初期化用
+bool EnemyCheckHitObj(void);	//敵の当たり判定
+void EnemyGameDraw(void);	//敵のゲームシーンでの描画用
