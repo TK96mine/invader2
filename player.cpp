3d925840f4272d89a81b7f1ef78834704d71853a
K <k91maad@outlook.jp>
2020-05-14 13:52:46 +0900

    2020/5/14  13:52更新

--- player.cpp
+++ player.cpp
@@ -11,19 +11,12 @@ int playerPosY;		//ﾌﾟﾚｲﾔｰのY座標
 int playerSpeed;	//ﾌﾟﾚｲﾔｰのｽﾋﾟｰﾄﾞ
 bool playerFlag;		//ﾌﾟﾚｲﾔｰのﾌﾗｸﾞ
 int playerLife;		//残機
-
-//爆発
-int playerbakuImage[BAKU_ANI];	//爆発画像格納用
-int playerbakuPosX;	//爆発画像の横軸位置
-int playerbakuPosY;	//爆発画像の縦軸位置
-int playerbakuAni;	//爆発のアニメーション用
-bool playerbakuFlag;	//爆発画像の状態
+int playerAniCnt;	//プレイヤーのアニメーション用カウンター
 
 void PlayerSystemInit(void)
 {
 	//プレイヤー
 	LoadDivGraph("image/serval.png", PLAYER_STYLE_MAX * PLAYER_ANI_MAX, PLAYER_ANI_MAX, PLAYER_STYLE_MAX, PLAYER_SIZE_X, PLAYER_SIZE_Y, playerImage[0]);
-	LoadDivGraph("image/blast.png", 24, 6, 4, BAKU_SIZE_X, BAKU_SIZE_Y, playerbakuImage);
 }
 
 void PlayerGameInit(void)
@@ -33,18 +26,11 @@ void PlayerGameInit(void)
 	playerSpeed = (PLAYER_DEF_SPEED);
 	playerFlag = true;
 	playerLife = PLAYER_DEF_LIFE;
-
-	//爆発
-	playerbakuPosX = playerPosX;
-	playerbakuPosY = playerPosY;
-	playerbakuAni = 0;
-	playerbakuFlag = false;
 }
 
 //プレイヤーの処理
 void PlayerControl(void)
 {
-
 	if (playerFlag == true)
 	{
 		//右移動
@@ -73,3 +59,121 @@ void PlayerControl(void)
 		}
 	}
 }
+
+bool PlayerCheckHitEnemy(void)
+{
+	for (int y = 0; y < ENEMY_Y; y++)
+	{
+		for (int x = 0; x < ENEMY_X; x++)
+		{
+			//敵と自機との当たり判定
+			if (EnemyFlag[y][x] == true && PlayerFlag == true)
+			{
+				if (EnemyPosX[y][x] + ENEMY_SIZE_X >= PlayerPosX
+					&& EnemyPosX[y][x] <= PlayerPosX + PLAYER_SIZE_X
+					&& EnemyPosY[y][x] + ENEMY_SIZE_Y >= PlayerPosY
+					&& EnemyPosY[y][x] <= PlayerPosY + PLAYER_SIZE_Y)
+				{
+					PlayerFlag = false;
+					return true;
+
+					/*else if (PlayerLife == 0)
+					{
+						systemScene = SCENE_GAMEOVER;
+					}*/
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+//プレイヤーと敵の弾の当たり判定
+bool PlayerCheckHitEShot(void)
+{
+	//敵の弾と自機との当たり判定
+	for (int e = 0; e < ESHOT_MAX; e++)
+	{
+		if (EShotFlag[e] == true && PlayerFlag == true)
+		{
+
+			if (EShotPosX[e] + ESHOT_SIZE_X >= PlayerPosX
+				&& EShotPosX[e] <= PlayerPosX + PLAYER_SIZE_X
+				&& EShotPosY[e] + ESHOT_SIZE_Y >= PlayerPosY
+				&& EShotPosY[e] <= PlayerPosY + PLAYER_SIZE_Y)
+			{
+				EShotFlag[e] = false;
+				PlayerFlag = false;
+				PlayerFlag = true;
+				PlayerDeathProc();
+
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+// 自機の減算処理
+void PlayerDeathProc(void)
+{
+	PlayerLife--;
+
+	if (PlayerLife > 0)
+	{
+		if (PlayerFlag == false)
+		{
+
+			for (int y = 0; y < ENEMY_Y; y++)
+			{
+				for (int x = 0; x < ENEMY_X; x++)
+				{
+					EnemyPosX[y][x] = ((ENEMY_SIZE_X * 10) / 7) * x;
+					EnemyPosY[y][x] = (ENEMY_SIZE_Y * 9 / 7) * y;
+					enemyType[y][x] = (ENEMY_TYPE)((y % (ENEMY_TYPE_MAX - 1)) + 1);
+				}
+			}
+			PlayerFlag = true;
+		}
+	}
+
+	if (PlayerLife <= 0)
+	{
+		systemScene = SCENE_GAMEOVER;		// 残機がない場合、ｹﾞｰﾑｵｰﾊﾞｰにする
+	}
+}
+
+void PlayerGameDraw(void)
+{
+	//残機数を表示
+	DrawFormatString((GAME_SCREEN_X + GAME_SCREEN_SIZE_X) + 50, 300, 0xFFFFFF, "PLAYER = %d", PlayerLife);
+
+	//プレイヤーの位置表示
+	DrawFormatString(0, 0, 0xFFFFFF, "PLAYER = ( %d, %d)", PlayerPosX, PlayerPosY);
+
+	//プレイヤーの残機表示
+	for (int p = 0; p < PlayerLife; p++)
+	{
+		DrawGraph((GAME_SIZE_X + 20) + 40 * (p % 3), (SCREEN_SIZE_Y - 80) - PLAYER_SIZE_Y * ((p / 3) + 1), PlayerImage[PLAYER_STYLE_FRONT][0], true);
+	}
+
+	//プレイヤーの表示
+	if (PlayerFlag == true)
+	{
+		if (playerAniCnt % 60 <= 30)
+		{
+			for (int x = 0; x < PLAYER_ANI_MAX; x++)
+			{
+				DrawGraph(PlayerPosX + GAME_OFFSET_X, (GAME_SIZE_Y + GAME_OFFSET_Y) - PLAYER_SIZE_Y, PlayerImage[PLAYER_STYLE_BACK][0], true);
+			}
+		}
+		else
+		{
+			DrawGraph(PlayerPosX + GAME_OFFSET_X, (GAME_SIZE_Y + GAME_OFFSET_Y) - PLAYER_SIZE_Y, PlayerImage[PLAYER_STYLE_BACK][1], true);
+		}
+	}
+}
+
+
