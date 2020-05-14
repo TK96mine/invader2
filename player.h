214d124e76f50b2a417d1dc4e78541832f734365
K <k91maad@outlook.jp>
2020-05-14 10:04:30 +0900

    player.hを追加

---
+++ player.h
@@ -0,0 +1,23 @@
+//定数
+#define PLAYER_SIZE_X 48	//ﾌﾟﾚｲﾔｰの横ｻｲｽﾞ
+#define PLAYER_SIZE_Y 48	//ﾌﾟﾚｲﾔｰの縦ｻｲｽﾞ
+#define PLAYER_DEF_SPEED 2	//ﾌﾟﾚｲﾔｰのｽﾋﾟｰﾄﾞ
+#define PLAYER_DEF_LIFE 5	//プレイヤーのデフォルトのライフ
+#define PLAYER_ANI_MAX 2	//プレイヤーのアニメーションする画像数
+
+//列挙型
+//プレイヤーの向き
+enum PLAYER_STYLE
+{
+	PLAYER_STYLE_BACK,
+	PLAYER_STYLE_FRONT,
+	PLAYER_STYLE_MAX
+};
+
+//構造体
+
+//プロトタイプ宣言
+void PlayerSystemInit(void);	//プレイヤーの画像読み込み専用
+void PlayerGameInit(void);	//プレイヤーの変数の初期化用
+void PlayerControl(void);	//プレイヤーの移動等の処理用
+
