9225d4950e49dcf110634805b909ec301123a414
K <k91maad@outlook.jp>
2020-05-14 14:06:45 +0900

    2020/5/14  14:06  shot.hを更新

--- shot.h
+++ shot.h
@@ -1,1 +1,13 @@
-#pragma once
+//定数
+
+//列挙型
+
+//構造体
+
+//プロトタイプ宣言
+void ShotSystemInit(void);	//弾の画像ロード用
+void ShotGameInit(void);	//弾の変数の初期化用
+void PlayerShotControl(void);	//プレイヤーの弾移動処理
+void EnemyShotControl(void);	//敵の弾の移動処理
+void ShotGameDraw(void);	//ゲームシーンでの弾の描画用
+
