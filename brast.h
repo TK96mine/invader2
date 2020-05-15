0cf2e325f29526e1ed88e858a0397f9f7229bca1
K <k91maad@outlook.jp>
2020-05-15 16:39:24 +0900

    2020/5/15 brast.hを更新

--- brast.h
+++ brast.h
@@ -6,6 +6,7 @@
 
 //プロトタイプ宣言
 void BrastSystemInit(void);	//爆発の画像読み込み処理用
-void BrastGameInit(void);	//爆発用変数の初期化用
+void PlayerBrastGameInit(XY playerPos);	//プレイヤーの爆発用変数の初期化用
+void EnemyBrastGameInit(void);	//敵の爆発用変数の初期化用
 void BrastControl(void);	//爆発処理用
 void BrastGameDraw(void);	//爆発の画像描画用
